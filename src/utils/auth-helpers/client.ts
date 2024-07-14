13bb6c8b06e9a1692cd7cefca9c98f6e56515d3f
joeBlockchain <94577719+joeBlockchain@users.noreply.github.com>
2024-07-13 22:01:35 -0700

    try using process.env

--- src/utils/auth-helpers/client.ts
+++ src/utils/auth-helpers/client.ts
@@ -34,7 +34,7 @@ export async function signInWithOAuth(e: React.FormEvent<HTMLFormElement>) {
 
   // Create client-side supabase client and call signInWithOAuth
   const supabase = createClient();
-  const redirectURL = encodeURIComponent(getURL('/auth/callback'));
+  const redirectURL = `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`;
   console.log("oauth redirect url", redirectURL);
   await supabase.auth.signInWithOAuth({
     provider: provider,
