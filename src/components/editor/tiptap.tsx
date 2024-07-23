328c6175b771e4fa09a36f7d2986436c5bf7969e
joeBlockchain <94577719+joeBlockchain@users.noreply.github.com>
2024-07-22 23:30:27 -0700

    basic styling for tiptap editor bubble and floating menus

--- src/components/editor/tiptap.tsx
+++ src/components/editor/tiptap.tsx
@@ -1,8 +1,34 @@
 "use client";
 
 import React, { useEffect, useCallback } from "react";
-import { useEditor, EditorContent } from "@tiptap/react";
+import {
+  BubbleMenu,
+  EditorContent,
+  FloatingMenu,
+  useEditor,
+} from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { Button } from "../ui/button";
+import { Toggle } from "../ui/toggle";
+import {
+  Bold,
+  Italic,
+  Strikethrough,
+  Minus,
+  Undo,
+  Redo,
+  Heading1,
+  Heading2,
+  List,
+} from "lucide-react";
+import { CommandShortcut } from "@/components/ui/command";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 interface TiptapProps {
   initialContent: string;
@@ -30,7 +56,150 @@ const Tiptap: React.FC<TiptapProps> = ({ initialContent, onChange }) => {
     }
   }, [editor, initialContent]);
 
-  return <EditorContent editor={editor} />;
+  return (
+    <>
+      {editor && (
+        <BubbleMenu
+          className="flex border bg-background rounded-xl shadow-md p-1 items-center justify-center gap-1 w-fit"
+          tippyOptions={{ duration: 100 }}
+          editor={editor}
+        >
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Toggle
+                  size="sm"
+                  onClick={() => editor.chain().focus().toggleBold().run()}
+                  pressed={editor.isActive("bold")}
+                >
+                  <Bold className="w-4 h-4" />
+                </Toggle>
+              </TooltipTrigger>
+              <TooltipContent className="flex flex-row gap-4 items-center">
+                <p>Bold</p>
+                <CommandShortcut>⌘B</CommandShortcut>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Toggle
+                  size="sm"
+                  onClick={() => editor.chain().focus().toggleItalic().run()}
+                  pressed={editor.isActive("italic")}
+                >
+                  <Italic className="w-4 h-4" />
+                </Toggle>
+              </TooltipTrigger>
+              <TooltipContent className="flex flex-row gap-4 items-center">
+                <p>Italic</p>
+                <CommandShortcut>⌘I</CommandShortcut>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Toggle
+                  size="sm"
+                  onClick={() => editor.chain().focus().toggleStrike().run()}
+                  pressed={editor.isActive("strike")}
+                >
+                  <Strikethrough className="w-4 h-4" />
+                </Toggle>
+              </TooltipTrigger>
+              <TooltipContent className="flex flex-row gap-4 items-center">
+                <p>Strikethrough</p>
+                <CommandShortcut>⌘S</CommandShortcut>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Toggle
+                  size="sm"
+                  onClick={() =>
+                    editor.chain().focus().setHorizontalRule().run()
+                  }
+                  pressed={editor.isActive("horizontalRule")}
+                >
+                  <Minus className="w-4 h-4" />
+                </Toggle>
+              </TooltipTrigger>
+              <TooltipContent className="flex flex-row gap-4 items-center">
+                <p>Horizontal Rule</p>
+                <CommandShortcut>⌘-</CommandShortcut>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Toggle
+                  variant="outline"
+                  size="sm"
+                  onClick={() => editor.chain().focus().undo().run()}
+                  disabled={!editor.can().chain().focus().undo().run()}
+                >
+                  <Undo className="w-4 h-4" />
+                </Toggle>
+              </TooltipTrigger>
+              <TooltipContent className="flex flex-row gap-4 items-center">
+                <p>Undo</p>
+                <CommandShortcut>⌘Z</CommandShortcut>
+              </TooltipContent>
+            </Tooltip>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Toggle
+                  variant="outline"
+                  size="sm"
+                  onClick={() => editor.chain().focus().redo().run()}
+                  disabled={!editor.can().chain().focus().redo().run()}
+                >
+                  <Redo className="w-4 h-4" />
+                </Toggle>
+              </TooltipTrigger>
+              <TooltipContent className="flex flex-row gap-4 items-center">
+                <p>Redo</p>
+                <CommandShortcut>⌘Y</CommandShortcut>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </BubbleMenu>
+      )}
+
+      {editor && (
+        <FloatingMenu
+          className="floating-menu bg-background border border-border p-1 rounded-xl items-center"
+          tippyOptions={{ duration: 100 }}
+          editor={editor}
+        >
+          <Toggle
+            size="sm"
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 1 }).run()
+            }
+            pressed={editor.isActive("heading", { level: 1 })}
+          >
+            <Heading1 className="w-4 h-4" />
+          </Toggle>
+          <Toggle
+            size="sm"
+            onClick={() =>
+              editor.chain().focus().toggleHeading({ level: 2 }).run()
+            }
+            pressed={editor.isActive("heading", { level: 2 })}
+          >
+            <Heading2 className="w-4 h-4" />
+          </Toggle>
+          <Toggle
+            size="sm"
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+            pressed={editor.isActive("bulletList")}
+          >
+            <List className="w-4 h-4" />
+          </Toggle>
+        </FloatingMenu>
+      )}
+
+      <EditorContent editor={editor} />
+    </>
+  );
 };
 
 export default Tiptap;
