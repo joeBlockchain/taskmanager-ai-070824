7c04026db4ab502d917cf5c7f26d4d28ee2a0fd6
joeBlockchain <94577719+joeBlockchain@users.noreply.github.com>
2024-07-22 21:11:07 -0700

    install tailwindtypography for styling in tiptap

--- src/components/editor/tiptap.tsx
+++ src/components/editor/tiptap.tsx
@@ -17,25 +17,12 @@ const Tiptap: React.FC<TiptapProps> = ({ initialContent, onChange }) => {
     editorProps: {
       attributes: {
         class:
-          "prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none",
+          "prose prose-base prose-neutral dark:prose-invert focus:outline-none max-w-none",
       },
     },
   });
 
-  return (
-    <div className="tiptap-container">
-      <style jsx global>{`
-        .tiptap-container .ProseMirror {
-          outline: none;
-        }
-        .tiptap-container .ProseMirror:focus {
-          outline: none;
-          box-shadow: none;
-        }
-      `}</style>
-      <EditorContent editor={editor} />
-    </div>
-  );
+  return <EditorContent editor={editor} />;
 };
 
 export default Tiptap;
