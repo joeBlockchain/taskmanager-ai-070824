c91ff4115c622cb0205247143d07a6bfb746cf14
joeBlockchain <94577719+joeBlockchain@users.noreply.github.com>
2024-07-26 19:25:11 -0700

    enable coffee tracker

--- src/app/(workspace)/workspace/projects/layout.tsx
+++ src/app/(workspace)/workspace/projects/layout.tsx
@@ -1,9 +1,16 @@
 // app/(workspace)/workspace/projects/layout.tsx
 
 import AuthButton from "@/components/ui/auth-button";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, Coffee, CoffeeIcon } from "lucide-react";
 import Link from "next/link";
 import { PropsWithChildren } from "react";
+import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 
 export default function ApplicationLayout({ children }: PropsWithChildren) {
   return (
@@ -12,9 +19,13 @@ export default function ApplicationLayout({ children }: PropsWithChildren) {
       {/* <nav> </nav> */}
       <div className="flex flex-row items-center justify-between mb-4">
         <div className="flex flex-row items-center text-center space-x-4">
-          <h1 className="text-2xl font-bold text-center">Projects</h1>
+          <h1 className="hidden sm:block text-2xl font-bold text-center">
+            Projects
+          </h1>
         </div>
-        <AuthButton size="small" />
+        <div className="flex flex-row items-center text-center space-x-4">
+          <AuthButton size="small" />
+        </div>
       </div>
       {children}
       {/* <footer></footer> */}
--- src/components/ui/auth-button.tsx
+++ src/components/ui/auth-button.tsx
@@ -13,6 +13,14 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { ChevronDown, LogOut, User } from "lucide-react";
 import { ModeToggle } from "../mode-toggle";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { Progress } from "@/components/ui/progress";
+import { CoffeeIcon } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 
 type AuthButtonProps = {
   size?: "default" | "small" | "tiny";
@@ -36,11 +44,48 @@ export default async function AuthButton({
     return redirect("/");
   };
 
+  let { data: profiles, error: profilesError } = await supabase
+    .from("profiles")
+    .select("api_cost_chat");
+
+  console.log(profiles);
+
   const avatarSizeClass =
     size === "tiny" ? "w-5 h-5" : size === "small" ? "w-7 h-7" : "w-10 h-10";
 
+  // Calculate the API cost as a multiple of 5
+  // Calculate the API cost progress
+  const apiCost =
+    profiles && profiles[0]?.api_cost_chat ? profiles[0].api_cost_chat : 0;
+  const progressPercentage = (apiCost / 15) * 100; // 5 represents 100%
+  const displayValue = Math.round((progressPercentage / 100) * 1500); // Convert to a value out of 500
+
+  console.log(progressPercentage);
+
   return user ? (
     <div className="flex items-center gap-4">
+      <Popover>
+        <PopoverTrigger>
+          <Button
+            variant="outline"
+            className="hover:bg-secondary/40 flex w-fit"
+          >
+            <CoffeeIcon className="w-5 h-5 flex-none mr-4" />
+
+            <div className="hidden sm:block text-sm font-medium mr-4">
+              <Badge variant="outline">{progressPercentage.toFixed(0)}%</Badge>
+            </div>
+            <Progress value={progressPercentage} className="h-2 w-[100px]" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent>
+          Let me buy you a cup of coffee! You are{" "}
+          <Badge>{progressPercentage.toFixed(0)}%</Badge> of the way there...
+          <br />
+          <br />
+          Type in a few more AI tasks to see where we can improve!
+        </PopoverContent>
+      </Popover>
       <ModeToggle />
       <DropdownMenu>
         <DropdownMenuTrigger asChild className="w-full">
